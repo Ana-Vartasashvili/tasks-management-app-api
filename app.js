8daa5d4950f85de26bfdbdf5a6cc2666d8d9b094
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-03-11 14:59:17 +0400

    connect application to the database

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import { config } from 'dotenv'
+
+const app = express()
+
+config()
+const DATABASE_URL = process.env.DATABASE_CONNECTION_URL
+
+app.use(bodyParser.json())
+
+// app.use('/tasks', tasksRoutes)
+// app.use('/auth', authRoutes)
+
+try {
+  mongoose.connect(DATABASE_URL)
+  app.listen(8000)
+} catch (error) {
+  console.log(error)
+}
