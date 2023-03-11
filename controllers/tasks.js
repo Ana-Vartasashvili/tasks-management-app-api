ab56c31002c9538926e4a712bf602583fb1409de
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-03-11 16:34:06 +0400

    add a new route for adding a new task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,3 +12,27 @@ export const getTasks = async (req, res, next) => {
     next(error)
   }
 }
+
+export const createTask = async (req, res, next) => {
+  const tasks = await Task.find()
+
+  try {
+    const { name } = req.body
+    const task = new Task({
+      name,
+    })
+
+    const newTask = await task.save()
+
+    res.status(201).json({
+      message: 'New task created successfully',
+      task: newTask,
+      tasks,
+    })
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500
+    }
+    next(error)
+  }
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,5 +4,6 @@ import { getTasks, createTask } from '../controllers/tasks.js'
 const router = Router()
 
 router.get('/', getTasks)
+router.post('/', createTask)
 
 export default router
