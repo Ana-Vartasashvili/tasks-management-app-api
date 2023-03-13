d00154fe510f7b0107febb1895d95bb8ef658844
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-03-13 15:58:18 +0400

    login the user and create a token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   try {
@@ -26,6 +27,48 @@ export const signup = async (req, res, next) => {
     if (!error.statusCode) {
       error.statusCode = 500
       error.message = error._message
+    }
+    next(error)
+  }
+}
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      const error = new Error('A user with this email could not be found!')
+      error.statusCode = 401
+      throw error
+    }
+
+    const passwordsMatch = await bcrypt.compare(password, user.password)
+
+    if (!passwordsMatch) {
+      const error = new Error('Wrong password!')
+      error.statusCode = 401
+      throw error
+    }
+
+    const token = jwt.sign(
+      {
+        email: user.email,
+        userId: user._id.toString(),
+      },
+      process.env.JWT_ACCESS_TOKEN,
+      {
+        expiresIn: '2h',
+      }
+    )
+
+    res.status(200).json({
+      token,
+      user,
+    })
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500
     }
     next(error)
   }
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { signup } from '../controllers/auth.js'
+import { signup, login } from '../controllers/auth.js'
 import signupSchema from '../schemas/signupSchema.js'
 import validateRequest from '../middlewares/validateRequest.js'
 
 const router = Router()
 
 router.post('/signup', signupSchema, validateRequest, signup)
+router.post('/login', login)
 
 export default router
